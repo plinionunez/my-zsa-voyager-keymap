ccd06f153aa0d37ab04a09410c42b4b05f0635bb
Chih-Wei Chang <2840571+lazywei@users.noreply.github.com>
2025-08-29 19:18:57 -0700

    no need to self define chordal hold

--- BRyqO/cwc.c
+++ BRyqO/cwc.c
@@ -1,39 +1,8 @@
 #ifdef CHORDAL_HOLD
-#define LAYOUT_LR(                                     \
-    k00, k01, k02, k03, k04, k05,                      \
-    k10, k11, k12, k13, k14, k15,                      \
-    k20, k21, k22, k23, k24, k25,                      \
-    k30, k31, k32, k33, k34, k35,                      \
-                             k40, k41,                 \
-                                                       \
-                         k50, k51, k52, k53, k54, k55, \
-                         k60, k61, k62, k63, k64, k65, \
-                         k70, k71, k72, k73, k74, k75, \
-                         k80, k81, k82, k83, k84, k85, \
-                    k90, k91)                          \
-    LAYOUT(k00, k01, k02, k03, k04, k05,   k50, k51, k52, k53, k54, k55, \
-           k10, k11, k12, k13, k14, k15,   k60, k61, k62, k63, k64, k65, \
-           k20, k21, k22, k23, k24, k25,   k70, k71, k72, k73, k74, k75, \
-           k30, k31, k32, k33, k34, k35,   k80, k81, k82, k83, k84, k85, \
-                               k40, k41,   k90, k91)
-// Handedness for Chordal Hold.
-const char chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] PROGMEM =
-  LAYOUT_LR(
-  '*'    , '*'    , '*'    , '*'    , '*'    , '*'    ,
-  '*'    , 'L'    , 'L'    , 'L'    , 'L'    , 'L'    ,
-  '*'    , 'L'    , 'L'    , 'L'    , 'L'    , 'L'    ,
-  '*'    , 'L'    , 'L'    , 'L'    , 'L'    , 'L'    ,
-                                               'L'    , 'L'    ,
-
-                    '*'    , '*'    , '*'    , '*'    , '*'    , '*'    ,
-                    'R'    , 'R'    , 'R'    , 'R'    , 'R'    , '*'    ,
-                    'R'    , 'R'    , 'R'    , 'R'    , 'R'    , '*'    ,
-                    'R'    , 'R'    , 'R'    , 'R'    , 'R'    , '*'    ,
-           'R'    , '*'
-);
-#endif  // CHORDAL_HOLD
+#endif // CHORDAL_HOLD
 //
-// bool get_chordal_hold(uint16_t tap_hold_keycode, keyrecord_t* tap_hold_record,
+// bool get_chordal_hold(uint16_t tap_hold_keycode, keyrecord_t*
+// tap_hold_record,
 //                       uint16_t other_keycode, keyrecord_t* other_record) {
 //   // Exceptionally consider the following chords as holds, even though they
 //   // are on the same hand in Dvorak.
@@ -49,11 +18,12 @@ const char chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] PROGMEM =
 //       if (other_keycode == MT(MOD_RCTL, KC_SCLN)) { return true; }
 //       break;
 //   }
-// 
+//
 //   // Also allow same-hand holds when the other key is in the rows below the
 //   // alphas. I need the `% (MATRIX_ROWS / 2)` because my keyboard is split.
-//   // if (tap_hold_record->event.key.row % (MATRIX_ROWS / 2) >= 5) { return true; }
-// 
+//   // if (tap_hold_record->event.key.row % (MATRIX_ROWS / 2) >= 5) { return
+//   true; }
+//
 //   // Otherwise defer to the opposite hands rule.
 //   return get_chordal_hold_default(tap_hold_record, other_record);
 // }
@@ -86,7 +56,8 @@ bool achordion_chord(uint16_t tap_hold_keycode,
 
   // Also allow same-hand holds when the other key is in the rows below the
   // alphas. I need the `% (MATRIX_ROWS / 2)` because my keyboard is split.
-  // if (tap_hold_record->event.key.row % (MATRIX_ROWS / 2) >= 5) { return true; }
+  // if (tap_hold_record->event.key.row % (MATRIX_ROWS / 2) >= 5) { return true;
+}
 
   return achordion_opposite_hands(tap_hold_record, other_record);
 }
